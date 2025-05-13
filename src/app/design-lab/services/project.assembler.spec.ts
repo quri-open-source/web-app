014dc298f7ace700d774a9a75ea92fff16efad59
Saurius45004004 <kaeltasblack@gmail.com>
2025-05-13 02:42:56 -0500

    feat: add ProjectAssembler for converting ProjectResponse to Project entity

---
+++ src/app/design-lab/services/project.assembler.spec.ts
@@ -0,0 +1,7 @@
+import { ProjectAssembler } from './project.assembler';
+
+describe('ProjectAssembler', () => {
+  it('should create an instance', () => {
+    expect(new ProjectAssembler()).toBeTruthy();
+  });
+});
