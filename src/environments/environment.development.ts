c72eaef717ad47042bee324bd97021cce54d3bcd
JonatanFD <u20211b995@upc.edu.pe>
2025-05-13 03:47:55 -0500

    feat: add development environment configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production : false,
+  projectsApiBaseUrl: 'http://localhost:3000/projects',
+  cloudinaryApiBaseUrl: 'https://api.cloudinary.com/v1_1/dkkfv72vo/image',
+};
