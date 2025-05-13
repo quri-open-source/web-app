09480abdff34d678b797cddca68f86bb3275dc9a
Fabrizio Contreras <fabriziocpa1516@gmail.com>
2025-05-13 02:30:28 -0500

    feat: add Canvas entity class for managing canvas data and layers

---
+++ src/app/design-lab/model/canvas.entity.spec.ts
@@ -0,0 +1,7 @@
+import { CanvasEntity } from './canvas.entity';
+
+describe('CanvasEntity', () => {
+  it('should create an instance', () => {
+    expect(new CanvasEntity()).toBeTruthy();
+  });
+});
