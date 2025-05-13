fb6eb380ef44917eb6e26994eaed19a1766bbdec
JonatanFD <u20211b995@upc.edu.pe>
2025-05-13 04:09:56 -0500

    feat: update app layout and styles; modify component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'web-app' title`, () => {
+  it(`should have the 'web-app-open-source-final' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('web-app');
+    expect(app.title).toEqual('web-app-open-source-final');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, web-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, web-app-open-source-final');
   });
 });
