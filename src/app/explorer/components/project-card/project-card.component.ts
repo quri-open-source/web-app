9ebc9b5d845100c5149b1d18bbfd9b166ed6c4e4
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-28 08:23:52 -0500

    feat(explorer): implement project card UI with styling and data binding

--- src/app/explorer/components/project-card/project-card.component.ts
+++ src/app/explorer/components/project-card/project-card.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProjectEntity } from '../../model/project.entity';
+import { ProjectService } from '../../services/project.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-project-card',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.css'
 })
 export class ProjectCardComponent {
-
+  @Input() project!: ProjectEntity;
 }
