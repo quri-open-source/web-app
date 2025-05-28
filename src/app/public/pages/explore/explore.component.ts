848a78ce1f41b32d5ad7e248b8d84c4e370cfd47
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-28 08:24:51 -0500

    feat(explore): implement explore component layout with search bar and projects view

--- src/app/public/pages/explore/explore.component.ts
+++ src/app/public/pages/explore/explore.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { ProjectsViewComponent } from '../../../explorer/components/projects-view/projects-view.component';
+import { SearchBarComponent } from '../../../explorer/components/search-bar/search-bar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-explore',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ProjectsViewComponent, SearchBarComponent],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.css'
 })
