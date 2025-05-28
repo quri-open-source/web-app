3d7825d25aa623fe02e679b4442ad600767397e1
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-27 23:32:36 -0500

    feat(search-bar): update search bar component with basic ui

--- src/app/explorer/components/search-bar/search-bar.component.ts
+++ src/app/explorer/components/search-bar/search-bar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
