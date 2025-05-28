b7efa86f00a06d524bdaf49a0ca0d76eb2c71a55
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-28 12:14:42 -0500

    feat(projects-view): implement project listing with responsive grid layout and data fetching

--- src/app/explorer/components/projects-view/projects-view.component.ts
+++ src/app/explorer/components/projects-view/projects-view.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectCardComponent } from '../project-card/project-card.component';
+import { ProjectEntity } from '../../model/project.entity';
+import { ProjectService } from '../../services/project.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects-view',
-  imports: [],
+  imports: [CommonModule, ProjectCardComponent],
   templateUrl: './projects-view.component.html',
   styleUrl: './projects-view.component.css'
 })
-export class ProjectsViewComponent {
+export class ProjectsViewComponent implements OnInit {
+  projects: ProjectEntity[] = [];
 
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit() {
+    this.projectService.getPublicProjects().subscribe((data: ProjectEntity[]) => {
+      this.projects = data;
+    });
+  }
 }
