21912b2c3de9d77f5ed9f33fb34853ad929fe14d
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-27 20:50:24 -0500

    feat(explorer): add project class/entity for explorer with initial setup

---
+++ src/app/explorer/model/project.entity.spec.ts
@@ -0,0 +1,7 @@
+import { ProjectEntity } from './project.entity';
+
+describe('ProjectEntity', () => {
+  it('should create an instance', () => {
+    expect(new ProjectEntity()).toBeTruthy();
+  });
+});
