52460cc73a409ca52e338d351d34fc54572e5914
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-27 20:50:45 -0500

    feat(explorer): add projects view component

---
+++ src/app/explorer/components/projects-view/projects-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-view',
+  imports: [],
+  templateUrl: './projects-view.component.html',
+  styleUrl: './projects-view.component.css'
+})
+export class ProjectsViewComponent {
+
+}
