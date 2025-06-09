5a38d47aedd893459b7042dedd6a406435f852ac
JonatanFD <u20211b995@upc.edu.pe>
2025-06-09 11:39:15 -0500

    feat: implement UserAssembler and UserResponse interfaces for user data handling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'http://localhost:3000',
-  serverBaseUrl: 'http://localhost:3000',
   defaultPreviewImageUrl: 'https://picsum.photos/150/150',
   defaultProjectStatus: 'blueprint',
   defaultGarmentSize: 'M',
   defaultGarmentColor: '#FFFFFF',
   garmentColorImagesUrl: 'https://res.cloudinary.com/dkkfv72vo/image/upload/v1747000549/Frame_530_hfhrko.webp',
   // Base URL for the projects API https://68230d7eb342dce800507517.mockapi.io
+  devUser : "user-1",
 };
