686dd79b034014647577247e183826554025347e
JonatanFD <u20211b995@upc.edu.pe>
2025-06-09 11:39:09 -0500

    refactor: remove AuthService and UserAssembler; add ProfileAssembler for user profile handling

--- src/app/user-management/services/user-profile.response.ts
+++
@@ -1,8 +0,0 @@
-export interface UserResponse {
-  id: string;
-  name: string;
-  email: string;
-  created_at: string; // Optional, may not be present in all responses
-  password: string; // Optional, may not be present in all responses
-  projects: Array<{ id: string; name: string }>;
-}
