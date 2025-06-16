6bf96d58fd6fa01c93b2837b7257e5c7f9e82dd5
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-06-16 04:30:25 -0500

    feat(product-catalog): enhance project model with privacy, price, and likes attributes

--- src/app/design-lab/model/project.entity.ts
+++ src/app/design-lab/model/project.entity.ts
@@ -1,4 +1,5 @@
 import {
+    PROJECT_PRIVACY,
     GARMENT_COLOR,
     GARMENT_SIZE,
     PROJECT_GENDER,
@@ -11,13 +12,15 @@ export class Project {
     userId: string;
     name: string;
     previewImageUrl: string;
+    price: number;
+    likes: number;
+    privacy: PROJECT_PRIVACY;
     status: PROJECT_STATUS;
     gender: PROJECT_GENDER;
     garmentColor: GARMENT_COLOR;
     garmentSize: GARMENT_SIZE;
     lastModified: Date;
     createdAt: Date;
-
     layers: Layer[];
 
     constructor(
@@ -25,6 +28,9 @@ export class Project {
         userId: string,
         name: string,
         previewImageUrl: string,
+        price: number = 0,
+        likes: number = 0,
+        privacy: PROJECT_PRIVACY,
         status: PROJECT_STATUS,
         gender: PROJECT_GENDER,
         garmentColor: GARMENT_COLOR,
@@ -37,6 +43,9 @@ export class Project {
         this.userId = userId;
         this.name = name;
         this.previewImageUrl = previewImageUrl;
+        this.price = price;
+        this.likes = likes;
+        this.privacy = privacy;
         this.status = status;
         this.gender = gender;
         this.garmentColor = garmentColor;
