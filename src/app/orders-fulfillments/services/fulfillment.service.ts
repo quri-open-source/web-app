a29049739e1cc483f6fb71cca6beb9722ad8b3d8
fabriziocpa <fabrizio.contreras.peralta@gmail.com>
2025-06-15 21:38:03 -0500

    feat: implement ManufacturerOrdersService and enhance OrderService with new methods

--- src/app/orders-fulfillments/services/fulfillment.service.ts
+++ src/app/orders-fulfillments/services/fulfillment.service.ts
@@ -3,16 +3,14 @@ import { Observable, map } from 'rxjs';
 import { BaseService } from '../../shared/services/base.service';
 import { Fulfillment } from '../model/fulfillment.entity';
 import { FulfillmentAssembler } from './fulfillment.assembler';
-import {FulfillmentResponse} from './fulfillment.response';
-
-
+import { FulfillmentResponse } from './fulfillment.response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FulfillmentService extends BaseService<FulfillmentResponse> {
   constructor() {
-    super('/fulfillments');
+    super('fulfillments');
   }
 
   getAllFulfillments(): Observable<Fulfillment[]> {
@@ -24,5 +22,50 @@ export class FulfillmentService extends BaseService<FulfillmentResponse> {
       )
     );
   }
-
+  
+  getFulfillmentsByManufacturer(manufacturerId: string): Observable<Fulfillment[]> {
+    return this.http.get<FulfillmentResponse[]>(`${this.resourcePath()}?manufacturer_id=${manufacturerId}`).pipe(
+      map((response: FulfillmentResponse[]) =>
+        response.map(fulfillmentResponse =>
+          FulfillmentAssembler.toEntityFromResource(fulfillmentResponse)
+        )
+      )
+    );
+  }
+  
+  getFulfillmentsByOrder(orderId: string): Observable<Fulfillment[]> {
+    return this.http.get<FulfillmentResponse[]>(`${this.resourcePath()}?order_id=${orderId}`).pipe(
+      map((response: FulfillmentResponse[]) =>
+        response.map(fulfillmentResponse =>
+          FulfillmentAssembler.toEntityFromResource(fulfillmentResponse)
+        )
+      )
+    );
+  }
+  
+  getFulfillmentById(id: string): Observable<Fulfillment> {
+    return this.getById(id).pipe(
+      map((response: FulfillmentResponse) =>
+        FulfillmentAssembler.toEntityFromResource(response)
+      )
+    );
+  }
+  
+  createFulfillment(fulfillment: Partial<Fulfillment>): Observable<Fulfillment> {
+    const fulfillmentResponse = FulfillmentAssembler.toResourceFromEntity(fulfillment as Fulfillment);
+    return this.create(fulfillmentResponse).pipe(
+      map((response: FulfillmentResponse) =>
+        FulfillmentAssembler.toEntityFromResource(response)
+      )
+    );
+  }
+  
+  updateFulfillment(id: string, fulfillment: Partial<Fulfillment>): Observable<Fulfillment> {
+    const fulfillmentResponse = FulfillmentAssembler.toResourceFromEntity(fulfillment as Fulfillment);
+    return this.update(id, fulfillmentResponse).pipe(
+      map((response: FulfillmentResponse) =>
+        FulfillmentAssembler.toEntityFromResource(response)
+      )
+    );
+  }
 }
