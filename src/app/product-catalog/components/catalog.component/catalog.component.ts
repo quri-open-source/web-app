d4d06c2a660aa6617a1ba0d141f0a5732de84641
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-06-16 22:51:38 -0500

    feat(product-catalog): enhance product display with detailed information and dialog integration

--- src/app/product-catalog/components/catalog.component/catalog.component.ts
+++ src/app/product-catalog/components/catalog.component/catalog.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../model/product.entity';
-import {MatIcon} from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { ProductDetailDialogComponent } from '../product-detail-dialog/product-detail-dialog.component';
+import { MatIcon } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-catalog',
@@ -14,7 +17,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule,
     MatIconModule,
     MatCardModule,
-    MatIcon
+    MatIcon,
+    MatDialogModule
   ],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
@@ -22,13 +26,25 @@ import { MatButtonModule } from '@angular/material/button';
 export class CatalogComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService, private cdr: ChangeDetectorRef) {
-  }
+  constructor(
+    private productService: ProductService,
+    private cdr: ChangeDetectorRef,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit() {
     this.productService.getAllProductsWithProjects().subscribe((products) => {
       this.products = products;
       this.cdr.detectChanges();
+    });
+  }
+
+  openDetail(product: Product) {
+    this.dialog.open(ProductDetailDialogComponent, {
+      data: product,
+      backdropClass: 'custom-dialog-backdrop',
+      panelClass: 'custom-dialog-panel',
+      hasBackdrop: true
     });
   }
 }
