7837cdcd4e7153d73b4637218e12fa36b06c0da3
JonatanFD <u20211b995@upc.edu.pe>
2025-07-05 20:43:50 -0500

    feat: integrate Stripe payment processing with form and service implementation

---
+++ src/app/shared/components/checkout-form/payment-element.component.ts
@@ -0,0 +1,106 @@
+import { Component, inject, signal, ViewChild } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormBuilder, Validators } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+
+import {
+  injectStripe,
+  StripePaymentElementComponent
+} from 'ngx-stripe';
+import {
+  StripeElementsOptions,
+  StripePaymentElementOptions
+} from '@stripe/stripe-js';
+import { environment } from '../../../../environments/environment';
+
+@Component({
+  selector: 'ngstr-checkout-form',
+  templateUrl: './payment-element.component.html',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    StripePaymentElementComponent
+  ]
+})
+export class CheckoutFormComponent {
+  @ViewChild(StripePaymentElementComponent)
+  paymentElement!: StripePaymentElementComponent;
+
+  private readonly fb = inject(UntypedFormBuilder);
+
+  paymentElementForm = this.fb.group({
+    name: ['John Doe', [Validators.required]],
+    email: ['support@ngx-stripe.dev', [Validators.required]],
+    address: [''],
+    zipcode: [''],
+    city: [''],
+    amount: [2500, [Validators.required, Validators.pattern(/\d+/)]]
+  });
+
+  elementsOptions: StripeElementsOptions = {
+    locale: 'en',
+    clientSecret: environment.stripePublicKey,
+    appearance: {
+      theme: 'flat'
+    }
+  };
+
+  paymentElementOptions: StripePaymentElementOptions = {
+    layout: {
+      type: 'tabs',
+      defaultCollapsed: false,
+      radios: false,
+      spacedAccordionItems: false
+    }
+  };
+
+  // Replace with your own public key
+  stripe = injectStripe(environment.stripePublicKey);
+  paying = signal(false);
+
+  pay() {
+    if (this.paying() || this.paymentElementForm.invalid) return;
+    this.paying.set(true);
+
+    const {
+      name,
+      email,
+      address,
+      zipcode,
+      city
+    } = this.paymentElementForm.getRawValue();
+
+    this.stripe
+      .confirmPayment({
+        elements: this.paymentElement.elements,
+        confirmParams: {
+          payment_method_data: {
+            billing_details: {
+              name: name as string,
+              email: email as string,
+              address: {
+                line1: address as string,
+                postal_code: zipcode as string,
+                city: city as string
+              }
+            }
+          }
+        },
+        redirect: 'if_required'
+      })
+      .subscribe(result => {
+        this.paying.set(false);
+        if (result.error) {
+          // Show error to your customer (e.g., insufficient funds)
+          alert({ success: false, error: result.error.message });
+        } else {
+          // The payment has been processed!
+          if (result.paymentIntent.status === 'succeeded') {
+            // Show a success message to your customer
+            alert({ success: true });
+          }
+        }
+      });
+  }
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  stripePublicKey: "pk_test_51RhgXgEI4EuyF4l3A89XvUAwutbDGNtUsFua1yfnm8LoDESZjuNtk0db8sWigC0kJW2YplK5vMd6qq35VTV4B2rn00PqiuRimi",
   serverBaseUrl: 'http://localhost:8080' // Cambia esto por tu URL de producción cuando sea necesario
 };
