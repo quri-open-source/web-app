45983e754f3b522b45f30c6250ef823e30dd1fa7
zGIKS <166082577+zGIKS@users.noreply.github.com>
2025-07-06 12:43:28 -0500

    refactor(environment): remove deprecated development and production configuration files

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  stripePublicKey: "pk_test_51RhgXgEI4EuyF4l3A89XvUAwutbDGNtUsFua1yfnm8LoDESZjuNtk0db8sWigC0kJW2YplK5vMd6qq35VTV4B2rn00PqiuRimi",
-  serverBaseUrl: 'https://web-services-ydwf.onrender.com'
-};
