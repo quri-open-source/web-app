ebfb4db21dda221c3984bc1034b687310cb605c0
zGIKS <166082577+zGIKS@users.noreply.github.com>
2025-07-06 12:49:40 -0500

    fix(environment): revert to hardcoded Stripe public key and server base URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  stripePublicKey: process.env['STRIPE_PUBLIC_KEY'] || '',
-  serverBaseUrl: process.env['SERVER_BASE_URL'] || ''
+  stripePublicKey: 'pk_test_51RhgXgEI4EuyF4l3A89XvUAwutbDGNtUsFua1yfnm8LoDESZjuNtk0db8sWigC0kJW2YplK5vMd6qq35VTV4B2rn00PqiuRimi',
+  serverBaseUrl: 'https://web-services-ydwf.onrender.com'
 };
